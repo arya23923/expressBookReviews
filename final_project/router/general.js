d8e069f3a9a9bce8c0c213a98ab6daa5aefe82f3
Arya Gupta <artagupta239gmail.com>
2024-12-30 18:01:57 +0530

    fifth task done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -69,8 +69,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    if(books[isbn]){
+        return res.status(200).send(books[isbn]);
+    }
+    else{
+        return res.status(404).send({message : "book by index not found"});
+    }
 });
 
 module.exports.general = public_users;
